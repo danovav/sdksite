ff6e85fcd86d9f501420e81b381e679f52af604e
danovav <danova.v.it@gmail.com>
2026-05-10 23:15:57 +0300

    Some fixes

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Този сайт е генериран с помощта на ChatGPT 5.5.");
+}
+
+function toggleMenu() {
+    const menu = document.querySelector("nav");
+    menu.classList.toggle("open");
+}
